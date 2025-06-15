ed34057623f8e91c52b2099336843efe0325caa4
Sachin Walunjakar <walunjakarsachin@gmail.com>
2025-06-15 09:13:42 +0530

    used MapStruct for simplifying mapping between dto & entity

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.codewithmosh.store.controllers;
 
-import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.entities.UserDto;
+import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,18 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
   UserRepository userRepository;
+  UserMapper userMapper;
 
   @GetMapping("/")
-  public Iterable<User> getAllUsers() {
-    return userRepository.findAll();
+  public Iterable<UserDto> getAllUsers() {
+    return userRepository.findAll()
+      .stream()
+      .map(userMapper::toDto)
+      .toList();
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<User> getUser(@PathVariable Long id) {
+  public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
     var user = userRepository.findById(id).orElse(null);
     if(user == null) {
       return ResponseEntity.notFound().build();
     }
-    return ResponseEntity.ok(user);
+    return ResponseEntity.ok(userMapper.toDto(user));
   }
 }
---
+++ src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.mappers;
+
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.entities.UserDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+  // here method name doesn't mapper, only input & return type matter
+   UserDto toDto(User user);
+}
